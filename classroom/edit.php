2bd7a4a7633292c60c48c0c62e1962b3533d5572
Salma Tarek <165893106+salmatarek55@users.noreply.github.com>
2025-09-15 22:38:12 +0300

    Update edit.php

--- classroom/edit.php
+++ classroom/edit.php
@@ -17,26 +17,67 @@ if(!isset($_GET['Id'])){
        exit;
  }
 ?>
-<div class="row justify-content-center">
-    <div class="col-6 bg-info">
-        <h1>page edit Classroom <?php print  $classroom['name'] ?></h1>
-    </div>
-</div>
-<div class="row justify-content-center">
-    <div class="col-6">
+<style>
+  .card-custom {
+    border-radius: 15px;
+    box-shadow: 0 5px 15px rgba(0,0,0,0.1);
+  }
+  .card-title {
+    font-weight: bold;
+    color: #0d6efd;
+  }
+  .form-label {
+    font-weight: 600;
+  }
+  .input-group-text {
+    background-color: #0d6efd;
+    color: #fff;
+  }
+</style>
+
+<div class="row justify-content-center mt-5">
+  <div class="col-md-6">
+    <div class="card card-custom">
+      <div class="card-body p-4">
+        <h3 class="text-center mb-4 card-title">
+          <i class="fa-solid fa-chalkboard"></i> Edit Classroom: 
+          <span class="text-secondary"><?php print $classroom['name'] ?></span>
+        </h3>
+
         <form method="POST">
-            <div class="mb-3">
-                <label class="form-label">Name of Classroom</label>
-                <input type="text" value="<?php print $classroom['name'] ?>" name='name' class="form-control">
+          <div class="mb-3">
+            <label class="form-label">Name of Classroom</label>
+            <div class="input-group">
+              <span class="input-group-text"><i class="fa-solid fa-door-open"></i></span>
+              <input 
+                type="text" 
+                value="<?php print $classroom['name'] ?>" 
+                name="name" 
+                class="form-control"
+              >
             </div>
-            <div class="mb-3">
-                <label class="form-label">Number of Students</label>
-                <input type="number" value="<?php print $classroom['number_students'] ?>" class="form-control" name='number_students'>
+          </div>
+
+          <div class="mb-3">
+            <label class="form-label">Number of Students</label>
+            <div class="input-group">
+              <span class="input-group-text"><i class="fa-solid fa-users"></i></span>
+              <input 
+                type="number" 
+                value="<?php print $classroom['number_students'] ?>" 
+                name="number_students" 
+                class="form-control"
+              >
             </div>
-            <button type="submit" class="btn btn-primary">Submit</button>
+          </div>
+
+          <button type="submit" class="btn btn-primary w-100">
+            <i class="fa-solid fa-floppy-disk"></i> Save Changes
+          </button>
         </form>
+      </div>
     </div>
+  </div>
 </div>
-<?php
-   include_once "../footer.php";
-?>
+
+<?php include_once "../footer.php"; ?>
