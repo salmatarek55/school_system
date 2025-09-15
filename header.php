ed7bd13c4526dde0c7a5dc345cf1859f6f383de7
Salma Tarek <165893106+salmatarek55@users.noreply.github.com>
2025-09-15 17:42:21 +0300

    Update header.php

--- header.php
+++ header.php
@@ -1,6 +1,7 @@
 <?php
 session_start();
 ?>
+
 <!-- haeder start -->
 <!DOCTYPE html>
 <html lang="en">
@@ -10,80 +11,73 @@ session_start();
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="/school_system/assets/bootstrap.min.css">
     <link rel="stylesheet" href="/school_system/assets/style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css">
+    <style>
+        body {
+          overflow-x: hidden; 
+          padding-top: 70px;
+       }
+    </style>
 </head>
 
 <body>
-    <div class="container">
-        <!-- جايبنها من البوتستراب -->
-        <nav class="navbar navbar-expand-lg navbar-dark bg-primary fixed-top">
-            <div class="container-fluid">
-                <a class="navbar-brand" href="/school_system"><?php print  isset($_SESSION['username'])?$_SESSION['username']:"school system" ?></a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                        <li class="nav-item">
-                            <a class="nav-link active" aria-current="page" href="/school_system">Home</a>
-                        </li>
-                        <?php
-                        if(isset($_SESSION['username']))
-                            { ?>
-                                <li class="nav-item">
-                                       <a class="nav-link" href="/school_system/student">Student</a>
-                                </li>
-                          <?php
-                            }?>
-                        <?php    
-                        if(isset($_SESSION['username']))
-                            { ?>
-                                <li class="nav-item">
-                                       <a class="nav-link" href="/school_system/teacher">Teacher</a>
-                                </li>
-                          <?php
-                            }?>  
-                         <?php    
-                        if(isset($_SESSION['username']))
-                            { ?>
-                                <li class="nav-item">
-                                       <a class="nav-link" href="/school_system/classroom">classroom</a>
-                                </li>
-                          <?php
-                            }?>       
-        
-                        <li class="nav-item">
-                            <a class="nav-link" href="/school_system/users">User</a>
-                        </li>
+     <nav class="navbar navbar-expand-lg bg-primary fixed-top">
+        <div class="w-100 d-flex justify-content-between align-items-center px-3">
+            <a class="navbar-brand text-white" href="/school_system">
+                <?php print isset($_SESSION['username']) ? $_SESSION['username'] : "school system" ?>
+            </a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent">
+                <span class="navbar-toggler-icon"></span>
+            </button>
 
-                        <li class="nav-item dropdown">
-                            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                              Auth
-                            </a>
-                            <ul class="dropdown-menu">
-                              <?php
-                              if(!isset($_SESSION['username']))
-                                {?>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link text-white" aria-current="page" href="/school_system">
+                            <i class="fa-solid fa-graduation-cap"></i> Home
+                        </a>
+                    </li>
+
+                    <?php if(isset($_SESSION['username'])) { ?>
+                        <li class="nav-item">
+                            <a class="nav-link text-white" href="/school_system/student">Student</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link text-white" href="/school_system/teacher">Teacher</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link text-white" href="/school_system/classroom">Classroom</a>
+                        </li>
+                    <?php } ?>
+
+                    <li class="nav-item">
+                        <a class="nav-link text-white" href="/school_system/users/login.php">
+                            <i class="fa-solid fa-user"></i> User
+                        </a>
+                    </li>
+
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle text-white" href="#" role="button" data-bs-toggle="dropdown">
+                            Auth
+                        </a>
+                        <ul class="dropdown-menu">
+                            <?php if(!isset($_SESSION['username'])) { ?>
                                 <li><a class="dropdown-item" href="/school_system/users/login.php">Login</a></li>
                                 <li><a class="dropdown-item" href="/school_system/users/add.php">Register</a></li>
-                                <?php  }else{
-                                ?>
+                            <?php } else { ?>
                                 <li><a class="dropdown-item" href="/school_system/users/logout.php">Logout</a></li>
-                               <?php }?>
-                                <li>
-                                    <hr class="dropdown-divider">
-                                </li>
-                                <li><a class="dropdown-item" href="#">Something else here</a></li>
-                            </ul>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link disabled" aria-disabled="true">Disabled</a>
-                        </li>
-                    </ul>
-                    <form class="d-flex" role="search">
-                        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
-                        <button class="btn btn-outline-success" type="submit">Search</button>
-                    </form>
-                </div>
+                            <?php } ?>
+                        </ul>
+                    </li>
+                </ul>
+
+                <form class="d-flex" role="search">
+                    <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
+                    <button class="btn btn-outline-light" type="submit">
+                        <i class="fa-solid fa-magnifying-glass"></i>
+                    </button>
+                </form>
             </div>
-        </nav>
-        <!-- header end -->
+        </div>
+    </nav>
+    <!-- Navbar end -->
