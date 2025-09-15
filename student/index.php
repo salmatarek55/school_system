0ecc18598f7ffa1c33a3e7f95d2091337dc3660b
Salma Tarek <165893106+salmatarek55@users.noreply.github.com>
2025-09-15 23:20:52 +0300

    Update index.php

--- student/index.php
+++ student/index.php
@@ -4,9 +4,11 @@ $conect = new Conect();
 
 if (isset($_POST['Id'])) {
     if ($conect->delete('student_info', $_POST['Id'])) {
-        header('location:\school_system\student\index.php?delete=success');
+        header('location:/school_system/student/index.php?delete=success');
         exit;
-        // print "you delete record";
+    } else {
+        header('location:/school_system/student/index.php?delete=fail');
+        exit;
     }
 }
 
@@ -14,69 +16,78 @@ $studentData = $conect->select("student_info");
 include_once "../header.php";
 ?>
 <div class="row justify-content-center">
-    <div class="col-6 ">
-        <h1>welcome to student show</h1>
-    </div>
-</div>
+    <div class="col-8">
+        <div class="card shadow-lg rounded-3 p-4">
+            <h1 class="mb-4 text-center">Welcome to Student Show</h1>
 
-<div class="row justify-content-center mt-5">
-    <div class="col-6 text-center">
-        <a class="btn btn-success" href="\school_system\student\add.php">Add New Student</a>
-        <?php
-        if (isset($_GET['add']) && $_GET['add'] == "success") {
-            print "<div class='alert alert-success'> you add student success</div>";
-        }
-        if (isset($_GET['delete']) && $_GET['delete'] == "success") {
-            print "<div class='alert alert-danger'> you delete student success</div>";
-        }
-        if (isset($_GET['edit']) && $_GET['edit'] == "success") {
-            print "<div class='alert alert-info'> you update student success</div>";
-        }
-        ?>
-        <table class="table">
-            <thead>
-                <tr>
-                    <th scope="col">#</th>
-                    <th scope="col">First Name</th>
-                    <th scope="col">Last Name</th>
-                    <th scope="col">Email</th>
-                    <th scope="col">Age</th>
-                    <th scope="col">Add by</th>
-                    <th scope="col">Delete</th>
-                    <th scope="col">Edit</th>
+            <a class="btn btn-success mb-3" href="/school_system/student/add.php">Add New Student</a>
 
-                </tr>
-            </thead>
-            <tbody>
-                <?php
-                // $i = 1;
-                foreach ($studentData as $key => $value) { ?>
+            <?php if (isset($_GET['add']) && $_GET['add'] == "success") { ?>
+                <div class="alert alert-success alert-dismissible fade show" role="alert">
+                    <i class="fa-solid fa-check-circle"></i> Student added successfully!
+                    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                </div>
+            <?php } ?>
+
+            <?php if (isset($_GET['delete']) && $_GET['delete'] == "success") { ?>
+                <div class="alert alert-danger alert-dismissible fade show" role="alert">
+                    <i class="fa-solid fa-trash"></i> Student deleted successfully!
+                    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                </div>
+            <?php } ?>
+
+            <?php if (isset($_GET['delete']) && $_GET['delete'] == "fail") { ?>
+                <div class="alert alert-warning alert-dismissible fade show" role="alert">
+                    <i class="fa-solid fa-triangle-exclamation"></i> Failed to delete student!
+                    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                </div>
+            <?php } ?>
+
+            <?php if (isset($_GET['edit']) && $_GET['edit'] == "success") { ?>
+                <div class="alert alert-info alert-dismissible fade show" role="alert">
+                    <i class="fa-solid fa-pen-to-square"></i> Student updated successfully!
+                    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                </div>
+            <?php } ?>
+
+            <table class="table table-striped table-bordered mt-3">
+                <thead class="table-dark">
                     <tr>
-                        <th scope="row"><?= $key + 1 ?></th>
-                        <td><?= $value['first_name'] ?></td>
-                        <td><?= $value['last_name'] ?></td>
-                        <td><?= $value['email'] ?></td>
-                        <td><?= $value['age'] ?></td>
-                        <td><?= @$conect->selectOne('users',$value['user_id']) ['user_name']?></td>
-                        <td>
-                            <form method="POST" onsubmit="return confirm('are you sure to delete student')">
-                                <input type="hidden" name="Id" value="<?= $value['Id'] ?>">
-                                <input type="submit" class="btn btn-danger" value="Delete">
-                            </form>
-                        </td>
-                        <td>
-                            <a class="btn btn-info" href="\school_system\student\edit.php?Id=<?= $value['Id'] ?>">Edit</a>
-                        </td>
+                        <th>#</th>
+                        <th>First Name</th>
+                        <th>Last Name</th>
+                        <th>Email</th>
+                        <th>Age</th>
+                        <th>Added by</th>
+                        <th>Delete</th>
+                        <th>Edit</th>
                     </tr>
-                <?php
-                    // $i++;
-                } ?>
-
-            </tbody>
-        </table>
+                </thead>
+                <tbody>
+                    <?php foreach ($studentData as $key => $value) { ?>
+                        <tr>
+                            <td><?= $key + 1 ?></td>
+                            <td><?= $value['first_name'] ?></td>
+                            <td><?= $value['last_name'] ?></td>
+                            <td><?= $value['email'] ?></td>
+                            <td><?= $value['age'] ?></td>
+                            <td><?= @$conect->selectOne('users', $value['user_id'])['user_name'] ?></td>
+                            <td>
+                                <form method="POST" onsubmit="return confirm('Are you sure you want to delete this student?')">
+                                    <input type="hidden" name="Id" value="<?= $value['Id'] ?>">
+                                    <input type="submit" class="btn btn-danger" value="Delete">
+                                </form>
+                            </td>
+                            <td>
+                                <a class="btn btn-info" href="/school_system/student/edit.php?Id=<?= $value['Id'] ?>">Edit</a>
+                            </td>
+                        </tr>
+                    <?php } ?>
+                </tbody>
+            </table>
+        </div>
     </div>
 </div>
 
-<!-- container close -->
-<?php include_once "../footer.php";
+<?php include_once "../footer.php"; ?>
 
